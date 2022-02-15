26bdb5f20965c59bfc9b821ff7dd5d1c15c5b61b
jerryjrmacapagal <jerryjrmacapagal@gmail.com>
2022-02-15 15:09:17 +0800

    02/15/2022

---
+++ Object Repository/Job search OR/Job search header.rs
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Job search header</name>
+   <tag></tag>
+   <elementGuidId>c26643cb-50e9-429a-85a4-547e8c204ba9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//h1[@class = 'job-search-title']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>job-search-title</value>
+   </webElementProperties>
+</WebElementEntity>
